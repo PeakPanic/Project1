7ccc7ef7446027aeddd370c4731cfc4cd5701f53
PeakPanic <Glenntheartist5@gmail.com>
2025-12-04 19:48:18 -0600

    Small submit changes

--- logic.py
+++ logic.py
@@ -11,6 +11,7 @@ class Logic(QMainWindow, Ui_VotingSheet):
         self.janeRadioButton.clicked.connect(self.toggleOtherTextBox)
         self.johnRadioButton.clicked.connect(self.toggleOtherTextBox)
         self.otherRadioButton.clicked.connect(self.toggleOtherTextBox)
+        self.submitButton.clicked.connect(self.submit)
 
 
     def toggleOtherTextBox(self):
@@ -28,3 +29,30 @@ class Logic(QMainWindow, Ui_VotingSheet):
         else:
             self.otherLineEdit.setVisible(False)
             self.otherLineEdit.setText('')
+
+    def submit(self):
+        '''
+        This Function is used to submit the candidate and id to a csv, unless the id boofs
+        '''
+        try:
+            int(self.idLineEdit.text())
+        except ValueError:
+            self.idLabel.setText('ID must consist of numbers')
+        else:
+            if len(self.idLineEdit.text()) != 5:
+                self.idLabel.setText('ID must be five (5) digits')
+            else:
+                with open('data.csv', 'w', newline='') as csvfile:
+                    if self.idLineEdit.text() in csv.reader(csvfile):
+                        self.idLabel.setText('ID has already voted')
+                    else:
+                        if self.janeRadioButton.isChecked():
+                            vote = 'Jane Doe'
+                        elif self.johnRadioButton.isChecked():
+                            vote = 'John Doe'
+                        elif self.otherRadioButton.isChecked():
+                            print('wow')
+                            # if len((self.otherLineEdit.text().strip())) != 2:
+                            #     self.idLabel.setText('Your candidate must have a first and last name')
+                            #     exit()
+
